66c19dde568eb983f59a363a48b880a5f803155e
Jalpesh Ahir <jalpeshahir6512@gmail.com>
2021-12-28 11:27:18 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        <h2>the CocktailDB</h2>
+        this project is devloped by Nandaniya Jalpesh
+      </p>
+    </section>
+  );
+}
